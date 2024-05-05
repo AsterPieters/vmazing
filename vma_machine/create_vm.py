77d25a824abb81b339943662278698a8635e79b0
AsterPieters <asterpieters@gmail.com>
2024-05-05 21:18:17 +0200

    added kubernetes doc

--- vma_machine/create_vm.py
+++ vma_machine/create_vm.py
@@ -12,22 +12,23 @@ sys.path.append('/opt/vmazing')
 # Custom modules
 from vma_storage.create_disk import create_disk
 
-def create_vm(vm_name, vm_memory, vm_vcpus, vm_storage, network):
+def create_vm(name, memory, vcpus, boot_disk_size, network):
     """
     Create a virtual machine.
     
     Args:
-        vm_name (str): Name of the virtual machine.
-        vm_memory (int): Ram size in Mb.
-        vm_vcpus (int): Amount of vcpu's
-        vm_storage (int): Amount of disksize
+        name (str): Name of the virtual machine.
+        memory (int): Ram size in Mb.
+        vcpus (int): Amount of vcpu's
+        boot_disk_size (int): Size of bootdisk
+        network (str): Name of the network to run in
     """
 
     # Create the disk path
-    disk_name = f"boot_disk_for_{vm_name}"
+    disk_name = f"boot_disk_for_{name}"
 
     # Create the disk
-    boot_disk = create_disk(disk_name, str(vm_storage))
+    boot_disk = create_disk(disk_name, str(boot_disk_size))
     if not boot_disk:
         return("Failed to create virtual machine.")
 
@@ -37,9 +38,9 @@ def create_vm(vm_name, vm_memory, vm_vcpus, vm_storage, network):
     # Create VM XML
     xml = f"""
     <domain type='kvm'>
-      <name>{vm_name}</name>
-      <memory unit='MiB'>{str(vm_memory)}</memory>
-      <vcpu placement='static'>{vm_vcpus}</vcpu>
+      <name>{name}</name>
+      <memory unit='MiB'>{str(memory)}</memory>
+      <vcpu placement='static'>{vcpus}</vcpu>
       <os>
         <type arch='x86_64' machine='pc-i440fx-2.12'>hvm</type>
         <boot dev='hd'/>
@@ -53,6 +54,7 @@ def create_vm(vm_name, vm_memory, vm_vcpus, vm_storage, network):
         <interface type='network'>
           <source network='{network}'/>
         </interface>
+        <graphics type='vnc' port='5900' autoport='yes' keymap='en-us'/>
       </devices>
     </domain>
     """
@@ -61,9 +63,9 @@ def create_vm(vm_name, vm_memory, vm_vcpus, vm_storage, network):
     dom = conn.createXML(xml, 0)
 
     if dom is not None:
-        print(f"Virtual machine {vm_name} created successfully.")
+        print(f"Virtual machine {name} created successfully.")
     else:
-        print(f"Failed to create virtual machine {vm_name}.")
+        print(f"Failed to create virtual machine {name}.")
 
     conn.close()
 
@@ -71,11 +73,11 @@ if __name__ == "__main__":
 
     # Take arguments
     parser = argparse.ArgumentParser(description="Create a virtual machine")
-    parser.add_argument("vm_name", help="Name of the virtual machine")
-    parser.add_argument("vm_memory", help="Ram size in Mb")
-    parser.add_argument("vm_vcpus", help="Amount of vcpu's")
-    parser.add_argument("vm_storage", help="Amount of disksize")
-    parser.add_argument("vm_network", help="Name of the network")
+    parser.add_argument("name", help="Name of the virtual machine")
+    parser.add_argument("memory", help="Ram size in Mb")
+    parser.add_argument("vcpus", help="Amount of vcpu's")
+    parser.add_argument("boot_disk_size", help="Amount of disksize")
+    parser.add_argument("network", help="Name of the network")
     args = parser.parse_args()
 
-    create_vm(args.vm_name, args.vm_memory, args.vm_vcpus, args.vm_storage, args.vm_network)
+    create_vm(args.name, args.memory, args.vcpus, args.boot_disk_size, args.network)
